3dcd44c2a91b050cf2bf6092eb350c895347e9d3
Abraham Tishelman-Charny <abraham.tishelman-charny@stonybrook.edu>
2015-11-05 12:46:54 -0500

    Inner mirrored copper strips added, still need to add final strip at max 'z' position, create terminals for new copper strips, create circuitry, and adjust domains for computation.

--- src/TPCMirror.java
+++ src/TPCMirror.java
@@ -22,21 +22,36 @@ public class TPCMirror extends TPC {
 		double r1 = TPCRadius;
 		double z2 = z1 + (FSELength+FSEzSpacing)/2;
 		double r2 = r1 + FSEThickness +FSErSpacing;
+		double r3 = innerTPCradius;
+		double z3 = z2;
+		double r4 = r3+r2-r1;
+		double z4 = z1;
 		
 		this.addRect("FSE1Rect",r1,z1,FSEThickness,FSELength);
+		this.addRect("FSE3Rect",r3,z3,FSEThickness,FSELength);
 		this.addRect("FSE2Rect",r2,z2,FSEThickness,FSELength);
-		this.makeFSEArray(2*offsetz(),new String[]{"FSE1Rect","FSE2Rect"},(FSENumber-1)/2);
+		this.addRect("FSE4Rect",r4,z4,FSEThickness,FSELength);
+		this.makeFSEArray(2*offsetz(),new String[]{"FSE1Rect","FSE2Rect","FSE3Rect","FSE4Rect"},(FSENumber-1)/2);
 		this.addRect("FSE"+FSENumber+"Rect", r1,z1+offsetz()*(FSENumber-1),FSEThickness,FSELength);
+		this.addRect("InnerFSE"+FSENumber+"Rect", r1,z1+offsetz()*(FSENumber-1),FSEThickness,FSELength);
 	}
 	
 	public void makeFSESelection(int actualNumber){
 		String name = "FSE"+actualNumber+"Selection";
+		String name1 = "InnerFSE"+actualNumber+"Selection";
 		
 		double rmin = TPCRadius - FSErSpacing/4;
 		if (actualNumber%2 == 1){
 			rmin = rmin + FSEThickness + FSErSpacing;
 		}
+		
+		double rmin2 = innerTPCradius + FSEThickness+FSErSpacing - FSErSpacing/4;
+		if (actualNumber%2 == 1){
+			rmin2 = rmin2 - FSEThickness - FSErSpacing;
+		}
+	
 		double rmax = rmin + FSEThickness + FSErSpacing/2;
+		double rmax2 = rmin2 + FSEThickness + FSErSpacing/2;
 		
 		double z1 = FSEzSpacing/2;
 
@@ -44,6 +59,7 @@ public class TPCMirror extends TPC {
 		double zmax = zmin + FSELength + FSEzSpacing/2;
 		
 		this.makeBoxSelection(name,rmin,zmin,rmax,zmax);
+		this.makeBoxSelection(name1,rmin2,zmin,rmax2,zmax);
 	}
 
 }
